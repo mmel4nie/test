ce326aeded6baceebb67252745878c04be9497fa
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-01-12 02:54:31 -0800

    Create speaker.js

---
+++ speaker.js
@@ -0,0 +1,44 @@
+import susieSharpe from "speakerImgs/ChristmasStar.png"
+
+speakerData = [
+    {
+        img: susieSharpe,
+        name: "Susie Sharpe",
+        description: <p>
+            Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
+            Her large-scale, acrylic paintings are inspired by the cosmos, 
+            music, nature, divine femininity, and the complexities in Black 
+            culture. Aramis has exhibited her colorful creations throughout 
+            Washington state. She also painted the “V” in Seattle’s Black 
+            Lives Matter mural. This self taught, Neddy Award winning-artist 
+            lets her imagination be her guide.
+        </p>
+    },
+    {
+        img: susieSharpe,
+        name: "Susie Sharpe",
+        description: <p>
+            Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
+            Her large-scale, acrylic paintings are inspired by the cosmos, 
+            music, nature, divine femininity, and the complexities in Black 
+            culture. Aramis has exhibited her colorful creations throughout 
+            Washington state. She also painted the “V” in Seattle’s Black 
+            Lives Matter mural. This self taught, Neddy Award winning-artist 
+            lets her imagination be her guide.
+        </p>
+    },
+    {
+        img: susieSharpe,
+        name: "Susie Sharpe",
+        description: <p>
+            Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
+            Her large-scale, acrylic paintings are inspired by the cosmos, 
+            music, nature, divine femininity, and the complexities in Black 
+            culture. Aramis has exhibited her colorful creations throughout 
+            Washington state. She also painted the “V” in Seattle’s Black 
+            Lives Matter mural. This self taught, Neddy Award winning-artist 
+            lets her imagination be her guide.
+        </p>
+    }
+];
+
